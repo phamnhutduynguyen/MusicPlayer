90368493f61c16291d9df335fb396582b6389d29
Nguyen Pham <gianguyengiaa3@gmail.com>
2021-07-11 18:16:00 +0700

    update v2

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ $(function()
 	},{
 		artist: "NIKI",
 		name: "Lowkey",
-		url: "Musics/lowkey.mp3",
+		url: "Musics/Lowkey.mp3",
 		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
 	},{
 		artist: "Little Mix",
@@ -36,9 +36,139 @@ $(function()
 		url: "Musics/Salute.mp3",
 		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
 	},{
-		artist: "Lee-PhuQuy",
-		name: "Minh Nguyệt Thiên Nhai",
-		url: "Musics/MinhNguyetThienNhai.mp3",
+		artist: "Britney Spears ft. Sabi",
+		name: "Beautiful",
+		url: "Musics/Beautiful.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Tape Machines",
+		name: "3D Print",
+		url: "Musics/3D Print.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Zedd & Katy Pery",
+		name: "365",
+		url: "Musics/365.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Sleepy",
+		name: "A Little Bit Crazy",
+		url: "Musics/A Little Bit Crazy.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Karmin",
+		name: "Acapella",
+		url: "Musics/Acapella.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Sleepy",
+		name: "All About Him",
+		url: "Musics/All About Him.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Madison Beer",
+		name: "Baby",
+		url: "Musics/Baby.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Mia Martina",
+		name: "Beast",
+		url: "Musics/Beast.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Ariana Grande",
+		name: "Bloodline",
+		url: "Musics/Bloodline.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Eurielle",
+		name: "City of The Dead",
+		url: "Musics/City of The Dead.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Ke$ha",
+		name: "C'mon",
+		url: "Musics/C'mon.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Frank Fitzpatrick",
+		name: "Come On Now",
+		url: "Musics/Come On Now.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Margaret",
+		name: "Cool Me Down",
+		url: "Musics/Cool Me Down.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Sabby Sousa",
+		name: "Cream n' Frosting",
+		url: "Musics/Cream n' Frosting.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Britney Spears",
+		name: "Criminal",
+		url: "Musics/Criminal.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Sleepy",
+		name: "Dance (The Way It Moves)",
+		url: "Musics/Dance (The Way It Moves).mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Sugar Blizz",
+		name: "Dear Friend",
+		url: "Musics/Dear Friend.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Ameryh",
+		name: "Deep End",
+		url: "Musics/Deep End.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Sleepy",
+		name: "FRIENDS",
+		url: "Musics/FRIENDS.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Jax Jones & Bebe Rexha",
+		name: "Harder",
+		url: "Musics/Harder.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Ellie Goulding & Juice WRLD",
+		name: "Hate Me",
+		url: "Musics/Hate Me.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Sleepy",
+		name: "Heist",
+		url: "Musics/Heist.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "MOUNTAINS & MACHINES",
+		name: "Hero",
+		url: "Musics/Hero.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Sleepy",
+		name: "Hey Mama",
+		url: "Musics/Hey Mama.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Sleepy",
+		name: "Hollow Mind",
+		url: "Musics/Hollow Mind.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "Alan Walker & K-391",
+		name: "Ignite",
+		url: "Musics/Ignite.mp3",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},{
+		artist: "HUGEL",
+		name: "WTF",
+		url: "Musics/WTF.mp3",
 		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
 	}];
 	//Hàm shuffle sẽ trộn thứ tự các item trong mãng theo thứ tự ngẫu nhiên. Dành cho bạn nào muốn nghe nhạc không theo thứ tự, các bài phát sẽ được phát ngẫu nhiên.
